dfd594307e177089873ee45969e87f39e75ccdec
gastonmatias <g.matiasvillagra@gmail.com>
2023-11-29 22:00:46 -0300

    mod - movie detail page

--- src/app/movies/pages/movie-detail-page/movie-detail-page.component.ts
+++ src/app/movies/pages/movie-detail-page/movie-detail-page.component.ts
@@ -5,19 +5,18 @@ import { MovieDetail } from '../../interfaces/movieDetail';
 
 @Component({
   templateUrl: './movie-detail-page.component.html',
-  styles: [
-  ]
+  styleUrls: ['./movie-detail-page.component.css']
 })
 export class MovieDetailPageComponent implements OnInit {
 
   public movie:MovieDetail | undefined;
   public idMovie:string = '1'
+  public isLoading:boolean = false
 
   constructor(
     private themoviedbService: ThemoviedbService,
     private route: ActivatedRoute
   ) { }
-
 
   ngOnInit(): void {
     const idPath = this.route.snapshot.url[0].path;
@@ -27,12 +26,20 @@ export class MovieDetailPageComponent implements OnInit {
   }
 
   getMovie(){
+    this.isLoading = true
     this.themoviedbService.getMovieById(+this.idMovie) //"+" convierte var id de tipo string a tipo numérico
       .subscribe( resp => {
-        this.movie = resp
-        console.log(this.movie)
-      }
+          this.movie = resp
+          this.isLoading = false
+        }
       )
+  }
+
+  getImageUrl(movie: any): string {
+    if (movie.poster_path===null) return '/assets/images/no_poster.png';
+    // if (movie.adult) return '/assets/images/sensitive_content2.png'; // descomentar para reemplazar imagen pixeleada por este asset (y comenntar el css ".censured-image")
+
+    return 'https://image.tmdb.org/t/p/w500/' + movie.poster_path;
   }
 
 }
